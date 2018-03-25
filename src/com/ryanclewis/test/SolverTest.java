341e71bdaede08c16839b9086da3b34d41c4076a
ryan-c-lewis <ryan.cp.lewis@gmail.com>
2018-03-25 17:51:21 -0600

    Tiny test refactor

--- src/com/ryanclewis/test/SolverTest.java
+++ src/com/ryanclewis/test/SolverTest.java
@@ -9,39 +9,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SolverTest {
 
-    @Test
-    void fourByFour() throws Exception {
-        Board board = BoardParser.parse(TestHelper.loadResource("board1.txt"));
+    private void assertSolutionIs(String resourceFileName, String solutionAsShortString) throws Exception {
+        Board board = BoardParser.parse(TestHelper.loadResource(resourceFileName));
         Board solvedBoard = new Solver().solve(board);
         assertEquals(
-                "1234341223414123",
+                solutionAsShortString,
                 solvedBoard.toShortString());
+    }
+
+    @Test
+    void fourByFour() throws Exception {
+        assertSolutionIs("board1.txt",
+                "1234341223414123");
     }
 
     @Test
     void nineByNine_noGuessing_1() throws Exception {
-        Board board = BoardParser.parse(TestHelper.loadResource("board2.txt"));
-        Board solvedBoard = new Solver().solve(board);
-        assertEquals(
-                "631492587518376924984725136743968215295187463127654398459813672862531749376249851",
-                solvedBoard.toShortString());
+        assertSolutionIs("board2.txt",
+                "631492587518376924984725136743968215295187463127654398459813672862531749376249851");
     }
 
     @Test
     void nineByNine_noGuessing_2() throws Exception {
-        Board board = BoardParser.parse(TestHelper.loadResource("board3.txt"));
-        Board solvedBoard = new Solver().solve(board);
-        assertEquals(
-                "247358916138697254956142378519863742863274591724915863492581637375426189681739425",
-                solvedBoard.toShortString());
+        assertSolutionIs("board3.txt",
+                "247358916138697254956142378519863742863274591724915863492581637375426189681739425");
     }
 
     @Test
     void nineByNine_guessingRequired() throws Exception {
-        Board board = BoardParser.parse(TestHelper.loadResource("board4.txt"));
-        Board solvedBoard = new Solver().solve(board);
-        assertEquals(
-                "892345176261479538754861329473586291915234867328917654536198742147652983689723415",
-                solvedBoard.toShortString());
+        assertSolutionIs("board4.txt",
+                "892345176261479538754861329473586291915234867328917654536198742147652983689723415");
     }
 }
